c4f10875c08ac24d76e77365a59e353341fa7c83
Paolovox <paolomondillo@gmail.com>
2016-07-15 17:27:01 +0200

    commit socket

---
+++ socket.h
@@ -0,0 +1,16 @@
+/*
+ * socket.h
+ *
+ *  Created on: 15 Jul 2016
+ *      Author: aPolo
+ */
+
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+#endif /* SOCKET_H_ */
+
+
+int create_win_socket();
+
+int create_linux_socket();
